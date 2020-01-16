1870badbbba7db31853ec750d855be72ea432de2
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-01-16 16:49:36 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1, y = 2, z = 3;
+            bool wynik;
+            wynik = !((x == 1 || y != 5) && z < 1);            // && has greater priority than ||
+            Console.WriteLine(wynik);
+            Console.ReadKey();
+        }
+    }
+}
